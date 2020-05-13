fd2f29deee3d3a372b408019fcf4f0e1af23e8fd
salpoz <salaam001@hotmail.com>
2020-05-13 17:45:38 +0500

    set onoff

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const ejs = require("ejs");
+const Gpio = require("onoff").Gpio;
+const light = Gpio(17, "out");
 const app = express();
 
-app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-const greet = "Simple Home Automation Project";
+const status = "Light is off!";
 
 app.get("/", (req, res) => {
-  res.render("main", { hello: greet });
+  res.render("main", { pstatus: status });
+});
+
+app.post("/", async (req, res) => {
+  const btn = await req.body.button;
+  if (btn === "on") {
+    light.write(1, (err) => {
+      res.render("main", { pstatus: "Light is ON!" });
+    });
+  } else {
+    light.write(
+      0,
+      (err = {
+        if(err) {
+          throw err;
+        }
+      })
+    );
+  }
 });
 
 app.listen(5000, () => {
